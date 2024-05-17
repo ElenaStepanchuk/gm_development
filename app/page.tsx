928e6e57d0560a6a1efdfa2ef3999ffff6e3c329
Elena <podolyak365@gmail.com>
2024-05-17 23:38:11 +0300

    init project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+import { Card } from "@/components";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Card />
+    </main>
+  );
+}
---
+++ components/card/Card.tsx
@@ -0,0 +1,11 @@
+import css from "./card.module.css";
+
+const Card = () => {
+  return (
+    <div>
+      <h1 className={css.card_title}>Card</h1>
+    </div>
+  );
+};
+
+export default Card;
