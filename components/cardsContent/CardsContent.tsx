834fdcb5a224966ab3f07329faf1cb1fdb4fa136
Elena <podolyak365@gmail.com>
2024-05-18 19:55:18 +0300

    finish styles

---
+++ components/cardsContent/CardsContent.tsx
@@ -0,0 +1,142 @@
+import image1 from "../../public/icons/1tap.svg";
+import image2 from "../../public/icons/2paint.svg";
+import image3 from "../../public/icons/3fame.svg";
+import image4 from "../../public/icons/4electrical.svg";
+import image5 from "../../public/icons/5door.svg";
+import image6 from "../../public/icons/6picture.svg";
+import image7 from "../../public/icons/7instalation.svg";
+import image8 from "../../public/icons/8home.svg";
+import image9 from "../../public/icons/9tap.svg";
+
+const CardsContent = [
+  {
+    icon: "icon1",
+    fill: "#179d2e",
+    iconWidth: "94",
+    iconHeight: "100",
+    title: "Plumbing",
+    list: [
+      "Install / Replace a Toilet",
+      "Install / Replace a Faucet",
+      "Fix a Leaking",
+      "Install / Replace a Sink",
+      "Repair Under Sink Plumbing",
+    ],
+    link: "https://www.example.com",
+  },
+  {
+    icon: "icon2",
+    fill: "#0177B8",
+    iconWidth: "91",
+    iconHeight: "100",
+    title: "Drywall & painting",
+    list: [
+      "Install Drywall",
+      "Patch and Paint Drywall",
+      "Paint a Wall / Trim",
+      "Paint or Stain A Fence",
+      "Paint a Cabinet Door",
+    ],
+    link: "https://www.example.com",
+  },
+  {
+    icon: "icon3",
+    fill: "#F5AF23",
+    iconWidth: "123",
+    iconHeight: "100",
+    title: "Furniture assembly",
+    list: [
+      "Assemble Fitness Equipment",
+      "Assemble - Custom Request",
+      "Assemble a TV Stand",
+      "Assemble Pet Furniture",
+      "Assemble a Bar Cabinet",
+    ],
+    link: "https://www.example.com",
+  },
+  {
+    icon: "icon4",
+    fill: "#F02E2C",
+    iconWidth: "113",
+    iconHeight: "101",
+    title: "Electrical",
+    list: [
+      "Install / Replace a Ceiling Fan",
+      "Replace Switch & Outlet Faceplate",
+      "Upgrade to a Video Doorbell",
+      "Install / Replace an Bathroom Exhaust Fan",
+    ],
+    link: "https://www.example.com",
+  },
+  {
+    icon: "icon5",
+    fill: "#179D2E",
+    iconWidth: "110",
+    iconHeight: "100",
+    title: "Doors & windows",
+    list: [
+      "Install / Replace New Door",
+      "Install / Replace a Screen Door",
+      "Install / Replace a Window Screen",
+      "Repair a Door Frame",
+    ],
+    link: "https://www.example.com",
+  },
+  {
+    icon: "icon6",
+    fill: "#0177B8",
+    iconWidth: "117",
+    iconHeight: "100",
+    title: "Hanging & mounting",
+    list: [
+      "Mount a TV",
+      "Remove a TV Mount from Wall",
+      "Mount and Hang a Projector Screen",
+      "Hang a Bathroom Fixture",
+    ],
+    link: "https://www.example.com",
+  },
+  {
+    icon: "icon7",
+    fill: "#F5AF23",
+    iconWidth: "98",
+    iconHeight: "100",
+    title: "Appliance installation",
+    list: [
+      "Replace an Existing Dishwasher",
+      "Replace an Existing Microwave",
+      "Replace an Existing Oven",
+    ],
+    link: "https://www.example.com",
+  },
+  {
+    icon: "icon8",
+    fill: "#F02E2C",
+    iconWidth: "116",
+    iconHeight: "100",
+    title: "Smart home",
+    list: [
+      "Install a Smart Lock",
+      "Install a Smart Home Security Camera",
+      "Mount a Baby Monitor Camera on the Wall",
+      "Install an Under Sink Water Filtration System",
+    ],
+    link: "https://www.example.com",
+  },
+  {
+    icon: "icon9",
+    fill: "#179D2E",
+    iconWidth: "94",
+    iconHeight: "100",
+    title: "Outdoor",
+    list: [
+      "Assemble an Outdoor Trampoline",
+      "Hang a Garage Shelving Unit",
+      "Install Outdoor Decorative Exterior Window Shutters",
+      "Install a Storm Door",
+    ],
+    link: "https://www.example.com",
+  },
+];
+
+export default CardsContent;
