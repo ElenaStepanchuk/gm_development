ae7729635d803a4b8587b86dd994fff5800628ae
Elena <podolyak365@gmail.com>
2024-05-18 20:38:11 +0300

    fix 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { basePath: "/test-task-in-next" };
+const nextConfig = { output: "export", basePath: "/test-task-in-next" };
 
 export default nextConfig;
